21cfe2f96be6214c42305d5052a0791b3a9411f4
Swapnil639 <swapnilpatil631997@gmail.com>
2023-05-16 09:08:20 +0530

    Added :Employee present or not

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,1 +1,9 @@
 console.log("Welcome to Employee wage program");
+
+const IS_PRESENT=1;
+let empCheck=Math.floor(Math.random()*2);
+if(empCheck==IS_PRESENT){
+    console.log("Employee is present");
+}else{
+    console.log("Employee is absent");
+}
