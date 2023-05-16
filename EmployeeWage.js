fad8d50f83991e9c2f34d9cd4ecf0e5a307472dc
Swapnil639 <swapnilpatil631997@gmail.com>
2023-05-16 09:18:30 +0530

    Added :Calculate Employee wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,9 +1,22 @@
 console.log("Welcome to Employee wage program");
 
-const IS_PRESENT=1;
-let empCheck=Math.floor(Math.random()*2);
-if(empCheck==IS_PRESENT){
-    console.log("Employee is present");
-}else{
-    console.log("Employee is absent");
-}
+const IS_PART_TIME=1;
+const IS_FULL_TIME=2;
+const PART_TIME_HOURS=4;
+const FULL_TIME_HOURS=8;
+const WAGE_PER_HOUR=20;
+let empHr=0;
+let empCheck=Math.floor(Math.random()*3);
+    switch (empCheck) {
+        case IS_PART_TIME:
+            empHr=PART_TIME_HOURS;            
+            break;
+        case IS_FULL_TIME:
+            empHr=FULL_TIME_HOURS;
+            break
+        default:
+            empHr=0;
+            break;
+    }
+    let empWage=empHr*WAGE_PER_HOUR;
+    console.log("Employee Wage is :"+empWage);
